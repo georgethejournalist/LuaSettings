2a0b1fafa89e2fdf0f6713b93d91e0e33da0acb5
GeorgeTheJournalist <24482750+georgethejournalist@users.noreply.github.com>
2020-07-21 14:42:10 +0200

    Added RenderSettings - an example of settings with enum value

---
+++ ExternalLibraryWithSettings/RenderSettings.cs
@@ -0,0 +1,19 @@
+﻿using LuaSettings;
+
+namespace ExternalLibraryWithSettings
+{
+    public enum RenderMode
+    {
+        None,
+        DirectX,
+        Vulkan
+    }
+
+    [Settings("RenderSettings")]
+    public class RenderSettings
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public RenderMode RenderMode { get; set; }
+    }
+}
